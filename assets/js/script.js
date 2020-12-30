6440c23ed386e92d5bbbd683e90eb6920b72f8dc
Mohammad Abu Mattar <mohammad.khaled@outlook.com>
2020-12-30 20:05:39 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -163,12 +163,9 @@ function yourMagicSquare() {
 	magicSquare();
 }
 
-
-/****************/
-
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function () {
-		navigator.serviceWorker.register('/sw.js').then(function (registration) {
+		navigator.serviceWorker.register('/Your-Magic-Square-and-Number/sw.js').then(function (registration) {
 			// Registration was successful
 			console.log('ServiceWorker registration successful with scope: ', registration
 				.scope);
